fb3450d2a8df051b6d99d459239f8daac6a389eb
Dmitry Astapov <dmitry@well-typed.com>
2010-12-11 21:51:32 +0200

    Comments in Mesh.Classes

--- src/Mesh/Classes.hs
+++ src/Mesh/Classes.hs
@@ -1,9 +1,11 @@
 {-# OPTIONS_GHC  -XTypeFamilies #-}
 
--- An exploratory typeclass for Meshes
-
+{-| An exploratory typeclass for Meshes.
+-}
 module Mesh.Classes (MeshFamily (..)) where
 
+-- | FIXME: does the processing of different meshes have enough in common to warrant a typeclass
+-- that would provide some useful abstraction?
 class MeshFamily m where
     data Space m     :: *
     data Partition m :: *
