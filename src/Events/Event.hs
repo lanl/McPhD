4920383ed17b4362e0c1af6a2e5d4e633fa424b8
Dmitry Astapov <dmitry@well-typed.com>
2010-12-11 21:43:30 +0200

    Commented Event.hs

--- src/Events/Event.hs
+++ src/Events/Event.hs
@@ -1,6 +1,13 @@
+{-| Datatypes for events generated during simulation.
+-}
 module Events.Event where
 
+-- | TODO: Why constructor and datatype have such drastically different names?
 data Motion   = Stream Double deriving Show
+
+-- | What has happened to the particle in the end
 data Endpoint = Scatter | Escape | Reflect deriving Show
 
-data Event = Event Motion Endpoint | NullEvent deriving Show
+data Event = Event Motion Endpoint -- ^ Ordinary event consists of series of motions (FIXME: correct?) + final event
+           | NullEvent -- ^ TODO: what is this? just for testing?
+           deriving Show
