e248412fcdf5ea103f6d0313f18210da8bf5733f
Dmitry Astapov <dmitry@well-typed.com>
2010-12-11 21:56:05 +0200

    Commented Streaming.hs

--- src/Streaming.hs
+++ src/Streaming.hs
@@ -1,6 +1,10 @@
+{-| Generic notion of streaming: we take something, simulate, and return generated event and new something
+-}
 module Streaming where
 
 import Events.Event
 
+-- | TODO: name like 'step' seems more appropriate, like in 'simulation step'.
+-- And class could be named Steppable. And module - Steppable.hs
 class Stream s where
   stream :: s -> Maybe (Event, s)
