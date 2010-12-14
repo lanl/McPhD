d28e20e37f7a94582921e5be31a47bc7fb1cdc00
Dmitry Astapov <dmitry@well-typed.com>
2010-12-14 12:33:30 +0200

    Renamed constructors in Fate (readability)

--- src/Events/Event.hs
+++ src/Events/Event.hs
@@ -13,9 +13,9 @@ data StepEnd = Scatter  -- ^ Particle is changing direction and energy.
              deriving Show
 
 -- | What stopped the particle streaming. Type parameter carries final particle value.
-data Fates p = Escape p     -- ^ Particle has escaped computational domain
-             | Absorption p -- ^ Particle has been absorbed into the material.
-             | Survived p   -- ^ Particle remains at end of the computational step.
+data Fate p = Escape p     -- ^ Particle has escaped computational domain
+            | Absorption p -- ^ Particle has been absorbed into the material.
+            | Survival p   -- ^ Particle remains at end of the computational step.
 
 data Event p = Event Motion StepEnd    -- ^ Ordinary event consists of a motion and a step end
              | Fates p                 -- ^ Final destination of the particle
