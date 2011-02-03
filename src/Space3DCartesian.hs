80f9db165e86cd0c97495861cc3a560cf3268331
Dmitry Astapov <dmitry@well-typed.com>
2011-02-03 12:19:12 +0200

    Using GeneralizedNewtypeDeriving to automatically derive instances of Approx. Removed unnecessary LANGUAGE pragmas and unused imports

--- src/Space3DCartesian.hs
+++ src/Space3DCartesian.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, TypeSynonymInstances, TypeFamilies, FlexibleInstances, UndecidableInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- Data types for Three-dimensonal space in Cartesian coordinates
 
 module Space3DCartesian(Distance (..)
@@ -21,8 +21,6 @@ import Data.Array.IArray
 import Approx
 import NumUnit
 
-import Control.Applicative
-
 -- * Data types
 
 -- | Coordinates
@@ -30,25 +28,25 @@ data Coord = X | Y | Z deriving (Show, Eq, Ord, Ix)
 -- coords = listArray (X,Z) "xyz" :: Array Coord Char
 
 -- | Position, a 3D vector
-newtype Position = Position { pos :: Vector3 } deriving (Eq, Show, Num)
+newtype Position = Position { pos :: Vector3 } deriving (Eq, Show, Num, Approx)
 -- | Momentum, a 3D vector
 
-newtype Momentum = Momentum { mom :: Vector3 } deriving (Eq, Show, Num)
+newtype Momentum = Momentum { mom :: Vector3 } deriving (Eq, Show, Num, Approx)
 
 -- | Motion, a 3D vector
-newtype Motion = Motion { mot :: Vector3 } deriving (Eq, Show, Num)
+newtype Motion = Motion { mot :: Vector3 } deriving (Eq, Show, Num, Approx)
 
 -- | Distance, scalar
-newtype Distance = Distance { dis :: Double  } deriving (Eq, Show, Num, Ord)
+newtype Distance = Distance { dis :: Double  } deriving (Eq, Show, Num, Ord, Approx)
 
 -- | Direction, a 3D vector of magnitude 1.
-newtype Direction = Direction { dir :: Vector3 } deriving (Eq, Show, Num)
+newtype Direction = Direction { dir :: Vector3 } deriving (Eq, Show, Num, Approx)
 
 -- | Time, elapsed time from beginning of streaming
-newtype Time = Time { time :: Double } deriving (Eq, Show, Num, Ord)
+newtype Time = Time { time :: Double } deriving (Eq, Show, Num, Ord, Approx)
 
 -- | A scalar representing the magnitude of velocity.
-newtype Speed = Speed { speed :: Double } deriving (Eq, Show, Num, Ord)
+newtype Speed = Speed { speed :: Double } deriving (Eq, Show, Num, Ord, Approx)
 
 
 -- * Construction and manupulation of space quantities
@@ -95,59 +93,3 @@ timeToDistance (Distance distance) (Speed speed) = Time (distance / speed)
 
 distanceToTime :: Time -> Speed -> Distance
 distanceToTime (Time time) (Speed speed) = Distance (time * speed)
-
-
--- * Nearness testing for space quantities. All of these dispatch to
--- the same test for the inner data type.
-
-instance Approx Position where
-    within_eps epsilon (Position a) (Position b)  = within_eps epsilon a b
-
-instance Approx Direction where
-    within_eps epsilon (Direction a) (Direction b) = within_eps epsilon a b
-
-instance Approx Distance where
-    within_eps epsilon (Distance a) (Distance b) = within_eps epsilon a b
-
-instance Approx Momentum where
-    within_eps epsilon (Momentum a) (Momentum b) = within_eps epsilon a b
-
-instance Approx Motion where
-    within_eps epsilon (Motion a) (Motion b) = within_eps epsilon a b
-
-instance Approx Time where
-  within_eps epsilon (Time a) (Time b) = within_eps epsilon a b
-
--- * Experimental Code:
-
--- | A class for things which wrap a value.
-class Wrapper w where
-    type Inner w
-    unwrap :: w -> Inner w
-
-instance Wrapper Position where
-    type Inner Position = Vector3
-    unwrap = pos
-
-instance Wrapper Momentum where
-    type Inner Momentum = Vector3
-    unwrap = mom
-
-instance Wrapper Distance where
-  type Inner Distance = Double
-  unwrap = dis
-
-
--- I'd like to make the newtypes, which are not parameterized, into
--- instances of Functor (and applicative?) to make the within_eps definitions simpler.
-
-newtype GenDistance a = GenDistance { genDis :: a }
-instance Functor GenDistance where
-    fmap f (GenDistance a) = GenDistance (f a)
-instance Applicative GenDistance where
-    pure = GenDistance
-    (<*>) (GenDistance f) (GenDistance a) = GenDistance (f a)
-
--- | Can I make class Wrapper a subclass of Functor?
-instance Approx Speed where
-  within_eps epsilon (Speed a) (Speed b) = within_eps epsilon a b
