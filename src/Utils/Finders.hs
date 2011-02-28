8998c04f3ae16e68f26ddca38f59e8cb52f14cef
Andres Loeh <andres@well-typed.com>
2011-02-28 13:24:53 +0100

    Rewritten Utils.Finders.mindex
    The new version is a one-liner making use of standard functions. (Actually, I'm
    not entirely sure if the function is really needed. Usually, if incides are
    really required, lists are not a good data structure, because they only have
    linear access to the list elements.)

--- src/Utils/Finders.hs
+++ src/Utils/Finders.hs
@@ -1,13 +1,8 @@
 module Utils.Finders where
 
 import Data.List
+import Data.Function
 
--- A function to find the minimum element of a list and return it with it's index.
+-- A function to find the minimum element of a list and return it with its index.
 mindex :: (Ord a) => [a] -> (Int, a)
-mindex [] = error "Empty list"
-mindex x = mindex' 0 x
-           where
-             mindex' :: (Ord a) => Int -> [a] -> (Int, a)
-             mindex' i (x:[]) = (i,x)
-             mindex' i (x:xs) = minimumBy (\a b -> snd a `compare` snd b) [(i, x), mindex' (i+1) xs]
-
+mindex = minimumBy (compare `on` snd) . zip [0..]
