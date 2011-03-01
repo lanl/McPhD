61cdb207ea1841c3819df71dfba092d4bcd1110a
tkelley <none@none>
2011-03-01 08:56:49 -0700

    Used sequenceT to simplify random number generator calls.
    Purified calls to samplePosition and sampleDirection.

--- basic/src/Source.hs
+++ basic/src/Source.hs
@@ -4,12 +4,14 @@
 -- (c) Copyright 2011 LANSLLC, all rights reserved
 
 module Source 
+    (genParticles)
     where
 
 import Particle
 import Physical
 import Mesh
 import Control.Applicative ((<$>))
+import Data.Tuple.Sequence (sequenceT)
 
 -- generates particles uniformly over a spatial domain
 genParticles :: Word32 ->   -- how many particles to generate
@@ -18,16 +20,17 @@ genParticles :: Word32 ->   -- how many particles to generate
                IO [Particle]
 genParticles 0 msh rng = return []
 genParticles n msh rng = do
-  (x,c) <- samplePosition msh rng
-  d <- sampleDirection msh rng
-  let p = Particle x d t e ew c rng tag
+  (ps1,ps2,ps3,ds1,ds2,ds3) <- sequenceT (rndm,rndm,rndm,rndm,rndm,rndm)
+  let (x,c) = samplePosition msh ps1 ps2 ps3
+      d     = sampleDirection msh ds1 ds2 ds3
+      p     = Particle x d t e ew c rng tag
   (p:) <$> (genParticles (n-1) msh rng)
   where -- d = Direction 1.0
         t = Time 1.0
         e = Energy 1.0
         ew = EnergyWeight 1.0
         tag = (n - 1)::Word32
-
+        rndm = random rng
 
 -- version
 -- $Id$
