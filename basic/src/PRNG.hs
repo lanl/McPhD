78392c3ab7fc9236ead1b1b81ee2a97342874026
tkelley <none@none>
2011-03-04 17:02:27 -0700

    Made getNRNs functions stricter.

--- basic/src/PRNG.hs
+++ basic/src/PRNG.hs
@@ -2,6 +2,7 @@
 -- T. M. Kelley
 -- Mar 03, 2011
 -- (c) Copyright 2011 LANSLLC, all rights reserved
+{-# LANGUAGE BangPatterns #-}
                                                                                 
 module PRNG (RNG
             ,random
@@ -16,20 +17,20 @@ import Numerical
 type RNG = StdGen
 
 getFiveRNs :: RNG -> (FP,FP,FP,FP,FP,RNG)
-getFiveRNs g = let (r5,g5) = random g4
-                   (r4,g4) = random g3
-                   (r3,g3) = random g2
-                   (r2,g2) = random g1
-                   (r1,g1) = random g
+getFiveRNs g = let (!r5,!g5) = random g4
+                   (!r4,!g4) = random g3
+                   (!r3,!g3) = random g2
+                   (!r2,!g2) = random g1
+                   (!r1,!g1) = random g
   in (r1,r2,r3,r4,r5,g5)
 
 getSixRNs :: RNG -> (FP,FP,FP,FP,FP,FP,RNG)
-getSixRNs g = let  (r6,g6) = random g5
-                   (r5,g5) = random g4
-                   (r4,g4) = random g3
-                   (r3,g3) = random g2
-                   (r2,g2) = random g1
-                   (r1,g1) = random g
+getSixRNs g = let  (!r6,!g6) = random g5
+                   (!r5,!g5) = random g4
+                   (!r4,!g4) = random g3
+                   (!r3,!g3) = random g2
+                   (!r2,!g2) = random g1
+                   (!r1,!g1) = random g
   in (r1,r2,r3,r4,r5,r6,g6)
 
 prand :: RNG
