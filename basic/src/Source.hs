20261b0294f364046d053101fcb24d97a2a5b528
tkelley <none@none>
2011-03-04 17:20:05 -0700

    Added genParticle, to generate a single particle and correctly propagate RNG state back to the caller.

--- basic/src/Source.hs
+++ basic/src/Source.hs
@@ -3,7 +3,8 @@
 -- Feb 15, 2011
 -- (c) Copyright 2011 LANSLLC, all rights reserved
 
-module Source (genParticles)
+module Source (genParticles
+              ,genParticle)
     where
 
 import Particle
@@ -25,6 +26,23 @@ genParticles n msh rng =
       d       = sampleDirection msh ds1 ds2 ds3
       p       = Particle x d t e ew c g1 tag
   in p:genParticles (n-1) msh g2
+  where t   = Time 1.0
+        e   = Energy 1.0
+        ew  = EnergyWeight 1.0
+        tag = n::Word32
+
+-- generates one particle uniformly over a spatial domain
+genParticle :: Word32 ->   -- particle tag
+                Mesh ->    
+                RNG ->
+                (Particle,RNG)
+genParticle n msh rng = 
+  let (ps1,ps2,ps3,ds1,ds2,ds3,rng') = getSixRNs rng 
+      (g1,g2) = split rng'
+      (x,c)   = samplePosition msh ps1 ps2 ps3
+      d       = sampleDirection msh ds1 ds2 ds3
+      p       = Particle x d t e ew c g1 tag
+  in (p,g2)
   where t   = Time 1.0
         e   = Energy 1.0
         ew  = EnergyWeight 1.0
