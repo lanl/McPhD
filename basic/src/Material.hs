2a77588c83f1fd338ec1988cf992daca22f2e3df
Timothy Kelley <tkelley@lanl.gov>
2011-04-26 11:15:04 -0600

    Updating to basic-rewrite.
    Removed Tag field from Particle.
    
    Material will now reside in Cell, so we remove the Material class, and rename MatState -> Material.

--- basic/src/Material.hs
+++ basic/src/Material.hs
@@ -1,32 +1,10 @@
--- Material.hs
--- T. M. Kelley
--- Jan 28, 2011
--- (c) Copyright 2011 LANSLLC, all rights reserved
+module Material where
 
-module Material (MatState(..)
-                ,Material(..)
-                -- ,simpleMat
-                )
-    where
-
-import Numerical
 import Physical
--- import Data.Array
 
--- | Material state in a cell
-data MatState = MatState {
-      sig_abs  :: !Opacity
-    , sig_scat :: !Opacity
-    , vel      :: !Velocity
-    , temp     :: !Temperature
-    } deriving (Show,Eq)
-
-
--- | Collection of material state information
-newtype Material = Material {mat :: Array CellIdx MatState} 
-
-
--- version
--- $Id$
-
--- End of file
+data Material = Material {
+    sig_abs  :: !Opacity
+  , sig_scat :: !Opacity
+  , vel      :: !Velocity
+  , temp     :: !Temperature
+  } deriving (Show, Eq)
--- basic/src/Particle.hs
+++ basic/src/Particle.hs
@@ -1,30 +1,15 @@
--- Particle.hs
--- T. M. Kelley
--- Jan 28, 2011
--- (c) Copyright 2011 LANSLLC, all rights reserved
-
--- definition of a particle
-
-module Particle ( Particle(..))
-  where
+module Particle where
 
 import Physical
 import PRNG
 
 data Particle = Particle {
-    pPos    :: !Position
-  , pDir    :: !Direction
-  , pTime   :: !Time
-  , pEnergy :: !Energy
-  , pWeight :: !EnergyWeight
-  , pCell   :: !CellIdx
-  , pRNG    :: !RNG
-  , pTag    :: !Tag
-  } deriving (Show)
-             
+    pos     :: !Position
+  , dir     :: !Direction
+  , time    :: !Time
+  , energy  :: !Energy
+  , weight  :: !EnergyWeight
+  , cell    :: !CellIdx
+  , rng     :: !RNG
+  }
 
-
--- version
--- $Id$
-
--- End of file
