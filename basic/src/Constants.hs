5c6520aed6f56ce759ca4d1cf660bebf32059efa
Timothy Kelley <tkelley@lanl.gov>
2011-04-26 10:57:56 -0600

    Updating to basic-rewrite.

--- basic/src/Constants.hs
+++ basic/src/Constants.hs
@@ -1,25 +1,16 @@
--- SimpleConstants.hs
--- T. M. Kelley
--- Dec 08, 2010
--- (c) Copyright 2010 LANSLLC all rights reserved.
-
 module Constants where
 
 import Numerical
 
--- constants 
-
--- speed of light in vacuum
--- c    = 2.9979e10 :: FP    -- cm/sec
-c :: FP 
+-- | Speed of light in vacuum.
+c :: FP
 c = 1.0 -- nice for testing
+-- c = 2.9979e10 -- cm/sec
 
--- limits for event selection
-tiny, huge :: FP
-tiny = 1e-300    
-huge = 1e+300     
+-- | Lower limit for event selection.
+tiny :: FP
+tiny = 1e-300
 
--- version
--- $Id$
-
--- End of file
+-- | Upper limit for event selection.
+huge :: FP
+huge = 1e+300
