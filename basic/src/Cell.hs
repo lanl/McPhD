35345e72d7b19df1e4ebb06395cdd1da376362b0
Timothy Kelley <tkelley@lanl.gov>
2011-04-26 10:54:24 -0600

    Adding Material to Cell--material properties have the same index set as mesh properties.

--- basic/src/Cell.hs
+++ basic/src/Cell.hs
@@ -1,35 +1,19 @@
--- Cell.hs
--- T. M. Kelley
--- Feb 09, 2011
--- (c) Copyright 2011 LANSLLC, all rights reserved
-
 module Cell where
 
 import Physical
+import Material
 
-data BoundaryCondition = Vac | Refl | Transp | None deriving (Show)
+data BoundaryCondition = Vac | Refl | Transp | None
+  deriving Show
 
-data BoundaryConditions = BCs { xbc :: BoundaryCondition
-                              , ybc :: BoundaryCondition
-                              , zbc :: BoundaryCondition} deriving (Show)
+data Cell = Cell {
+    lowB   :: Position
+  , highB  :: Position
+  , lowBC  :: BoundaryCondition
+  , highBC :: BoundaryCondition
+  , mat    :: Material -- QUESTION: ok to include here?
+  }
+  deriving Show
 
--- hope this works in 1D and 3D
-data CellProperties = CellProps {
-      lowB   :: Position -- claiming we cd rep a rectangular cell with two vertices
-    , highB  :: Position
-    , lowBC  :: BoundaryConditions
-    , highBC :: BoundaryConditions
-} deriving (Show)
-
-
-data Face = XLow | XHigh | YLow | YHigh | ZLow | ZHigh deriving (Show,Eq)
-
--- | convenience function: set up 1D B.C. without typing all the None's 
-bc1D :: BoundaryCondition -> BoundaryConditions
-bc1D bc = BCs bc None None
-
-
--- version
--- $Id$
-
--- End of file
+data Face = Lo | Hi -- QUESTION: all 1D, how to generalize
+  deriving (Eq, Show)
