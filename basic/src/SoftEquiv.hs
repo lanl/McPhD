455f1e27af8c71c10d96d01144f1b247bb161b0e
Timothy Kelley <tkelley@lanl.gov>
2011-04-26 12:51:58 -0600

    From basic-rewrite: adds infix softEquiv operator, specialization, and a standard tolerance.

--- basic/src/SoftEquiv.hs
+++ basic/src/SoftEquiv.hs
@@ -1,22 +1,25 @@
--- SoftEquiv.hs
--- T. M. Kelley
--- Jan 28, 2011
--- (c) Copyright 2011 LANSLLC, all rights reserved
-
 module SoftEquiv where
 
-{- Compare numbers for equality within some tolerance. 
- - The basic case is, if the absolute value of the difference of the 
- - value and the reference divided by the value is less than the tolerance,
- - evaluate True. If the value is zero, evaluate whether the reference is
- - less than the tolerance. -}
+import Numerical
+
+-- | Compare numbers for equality within some tolerance. The basic
+-- case is, if the absolute value of the difference of the value and
+-- the reference divided by the value is less than the tolerance,
+-- evaluate True. If the value is zero, evaluate whether the reference
+-- is less than the tolerance.
+{-# SPECIALIZE softEquiv :: FP -> FP -> FP -> Bool #-}
+{-# INLINE softEquiv #-}
 softEquiv :: (Num a, Ord a) => a -> a -> a -> Bool
-softEquiv val ref tol | val == 0       = abs ref < tol
-                      -- | abs(val) < tol = abs(ref) < tol
-                      | otherwise      = abs(val - ref) < tol * abs val
+softEquiv val ref tol
+  | val == 0  = abs ref < tol
+  -- | abs(val) < tol = abs(ref) < tol
+  | otherwise = abs (val - ref) < tol * abs val
 
+stdTol :: FP
+stdTol = 1e-11
 
--- version
--- $Id$
+{-# INLINE (~==~) #-}
+(~==~) :: FP -> FP -> Bool
+(~==~) = softEquiv stdTol
 
--- End of file
+infix 4 ~==~
