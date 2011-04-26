2310c9ae3741e278bac8343386d7047002878d8d
Timothy Kelley <tkelley@lanl.gov>
2011-04-26 12:41:03 -0600

    Adopting the basic-rewrite version, which makes the dist field strict.

--- basic/src/Event.hs
+++ basic/src/Event.hs
@@ -1,54 +1,67 @@
--- Event.hs
--- T. M. Kelley
--- Feb 09, 2011
--- (c) Copyright 2011 LANSLLC, all rights reserved
+module Event where
 
-module Event (Event(..)
-             ,isContinuing) 
-    where     
+import Data.List
+import Data.Ord
 
-import Physical
 import Cell
+import Physical
 
-{- Events: what can happen to a particle on a Monte Carlo step.
- - Scatter:  some type of scatter.  NB neutrinos have many more interaction 
- -           channels than photons: looks like ~6-8 per lepton, x2 for anti-nu's
- - Absorb:   physically a subset of scattering, computationally different in that
- -           absorption terminates a particle's flight
- - Transmit: reach a cell boundary and cross into a new mesh cell
- - Reflect:  reach a cell boundary and reflect from it
- - Escape:   reach a cell boundary and leave the problem domain
- - Census:   the particle's internal clock has reached the end of the time step;
- -           it is banked for the next time step.
- -
- - I've stuffed the momentum transfer into the Event. Otherwise, we need to 
- - compare successive events or particle states to compute this later.
- -}
-data Event = Scatter  { dist   :: FP          -- distance travelled
-                      , deltaP :: Momentum    -- momentum deposited  
-                      , eDep   :: EnergyWeight} -- energy deposited
-           | Absorb   { dist   :: FP 
-                      , deltaP :: Momentum 
-                      , eDep   :: EnergyWeight}
-           | Census   { dist   :: FP 
-                      , deltaP :: Momentum }
-           | Transmit { dist   :: FP 
-                      , face   :: Face }
-           | Escape   { dist   :: FP 
-                      , face   :: Face }
-           | Reflect  { dist   :: FP 
-                      , face   :: Face }
-             deriving (Show,Eq)
+-- | Events: what can happen to a particle on a Monte Carlo step.
+--
+-- Scatter:  some type of scatter; NB neutrions have more interaction
+--           channels than photons: loks like ~6-8 per lepton, x2 for
+--           anti-nu's
+-- Absorb:   physically a subset of scattering, computationally different
+--           in that absorption terminates a particle's flight
+-- Transmit: reach a cell boundary and cross into a new mesh cell
+-- Reflect:  reach a cell boundary and reflect from it
+-- Escape:   reach a cell boundary and leave the problem domain
+-- Census:   the particle's internal clock has reached the end of the
+--           time step; it is banked for the next time step.
+
+data Event =
+    Scatter  { dist   :: !FP            -- ^ distance travelled
+             , deltaP :: Momentum       -- ^ momentum deposited
+             , eDep   :: EnergyWeight   -- ^ energy deposited
+             }
+  | Absorb   { dist   :: !FP            -- ^ distance travelled
+             , deltaP :: Momentum       -- ^ momentum deposited
+             , eDep   :: EnergyWeight   -- ^ energy deposited
+             }
+  | Transmit { dist   :: !FP            -- ^ distance travelled
+             , face   :: Face           -- ^ which boundary
+             }
+  | Escape   { dist   :: !FP            -- ^ distance travelled
+             , face   :: Face           -- ^ which boundary
+             }
+  | Reflect  { dist   :: !FP            -- ^ distance travelled
+             , face   :: Face           -- ^ which boundary
+             }
+  | Census   { dist   :: !FP            -- ^ distance travelled
+             , deltaP :: Momentum       -- ^ momentum deposited
+             }
+  deriving (Show, Eq)
+-- REMARK: Only dist is strict. The reason is that we generate several
+-- events in each turn, and only select the one with the smallest
+-- distance. We don't actually want to perform the extra work for the
+-- events that are discarded.
+
+-- | Common type for all boundary event constructors.
+type BoundaryEvent = FP -> Face -> Event
 
 -- | Does an event continue a random walk?
 isContinuing :: Event -> Bool
-isContinuing evt = case evt of 
-                     Scatter {}  -> True
-                     Reflect {}  -> True
-                     Transmit {} -> True
-                     _           -> False
+isContinuing (Scatter  {}) = True
+isContinuing (Reflect  {}) = True
+isContinuing (Transmit {}) = True
+isContinuing _             = False
 
--- version
--- $Id$
+-- | Distance to be travelled for the event
+distance :: Event -> FP
+distance = dist
 
--- End of file
+-- | Determines the closest event; input list must contain at least
+-- one element.
+closestEvent :: [Event] -> Event
+closestEvent = minimumBy (comparing distance)
+
