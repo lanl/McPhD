8a977568ea229da38e7d4cfa11b6ce472a67724b
Timothy Kelley <tkelley@lanl.gov>
2011-04-29 16:42:42 -0600

    Removed QUESTION.

--- basic/src/Material.hs
+++ basic/src/Material.hs
@@ -3,8 +3,11 @@ module Material where
 import Physical
 
 data Material = Material {
-    sig_abs  :: !Opacity
-  , sig_scat :: !Opacity
-  , vel      :: !Velocity
-  , temp     :: !Temperature
+    sig_abs     :: !Opacity
+  , sig_scat    :: !Opacity
+  , vel         :: !Velocity    -- cm/sec
+  , tempE       :: !Temperature -- in energy units (MeV)
+  , rho_nucl    :: !Density     -- g/cc
+  , rho_e_minus :: !Density
+  , rho_e_plus  :: !Density
   } deriving (Show, Eq)
