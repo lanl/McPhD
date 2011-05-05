b052d7f2eff357d9cc9914b4aa4f3ca03c219c5c
Michael Buksas <mwbuksas@lanl.gov>
2011-05-05 17:36:16 -0600

    Adding prototype stream method to mainline code

--- src/Stream.hs
+++ src/Stream.hs
@@ -5,43 +5,19 @@ module Stream where
 import Particle.Classes
 import Data.List
 
--- Make a plain step function from context and the get environment
--- function
-addEnv :: con                        -- ^ A context
-          -> (con -> p -> env)       -- ^ Generate environment from context
-          -> (env -> p -> (evnt, p)) -- ^ The particle step function
-          -> p                       -- ^ Particle
-          -> (evnt , p)              -- ^ Resulting event and particle
-addEnv con env_fn step_fn p =
-  let get_env = env_fn con -- ^ closure for environment function
-  in step_fn (get_env p) p
 
+-- | Prototype events type without associated data
+data Event = Scatter | CellFace | Escape | Census
 
--- Attach a Done state to the particle which still carries the
--- particle. This way we can still get the last one.
-data Status p = Streaming p | Done p deriving (Show)
+isFinal :: Event -> Bool
+isFinal Escape {} = True
+isFinal Census {} = True
+isFinal _         = False
 
+step :: (Mesh m, Particle p) => m -> p -> (Event, p)
+step = undefined
 
--- Transform a particle step function + end tester into the maybe form.
-addStat :: (p -> (evnt, p))          -- ^ Plain step function
-           -> (evnt -> Bool)         -- ^ Test for final
-           -> Status p               -- ^ Particle with status
-           -> Maybe (evnt, Status p) -- ^ Maybe an event and new particle
-addStat _ _ (Done p) = Nothing  -- ^ Convert done particles to Nothing.
-addStat step final (Streaming p)
-  = let (e, p') = step p
-    in if (final e)
-       then Just (e, Done p')
-       else Just (e, Streaming p')
-
-
-stream :: con                        -- ^ A context
-          -> (con -> p -> env)       -- ^ Generate environment from context
-          -> (env -> p -> (evnt, p)) -- ^ The particle step function
-          -> (evnt -> Bool)          -- ^ Test for final events
-          -> p                       -- ^ An initial particle
-          -> [evnt]                  -- ^ Resulting events
-stream context get_env_fn step_fn final_fn p =
-  let step_env_fn = addEnv  context get_env_fn step_fn
-      step_status = addStat step_env_fn final_fn
-  in unfoldr step_status (Streaming p)
+stream :: Mesh -> Particle -> [(Event, Particle)]
+stream mesh particle = stream' mesh particle []
+    where stream' :: Mesh -> Particle -> [(Event, Particle)] -> [(Event, Particle)]
+          stream' mesh particle events = 
