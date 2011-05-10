fb7c032fa6db72a2cadf201c47ee65401380c41b
Timothy Kelley <tkelley@lanl.gov>
2011-05-10 16:06:14 -0600

    Added opacities function--computes list of individual opacities. Also replaced underscores with camel case.

--- basic/src/Opacity.hs
+++ basic/src/Opacity.hs
@@ -1,5 +1,5 @@
 {-| Opacity values: merger of microscopic cross section and macroscopic 
- -  material properties -}
+ -  material properties. -}
 
 module Opacity where
 
@@ -8,23 +8,32 @@ import Cell
 import Material
 import Physical
 
+
+-- | Compute individual opacities in one list.
+opacities :: Cell -> Energy -> Sigma.Lepton -> [Opacity]
+opacities c e sig = 
+  [ opNAbs c e
+  , opNElastic c e
+  , opEMinus c e sig
+  , opEPlus c e sig
+  ]
+
 -- | Total collision opacity
-op_collide :: Cell -> Energy -> Sigma.Lepton -> Opacity
-op_collide cell e_nu sigLep =
-  (op_N cell e_nu) + (op_lepton cell e_nu sigLep)
+opCollide :: Cell -> Energy -> Sigma.Lepton -> Opacity
+opCollide c e sigLep = sum $ opacities c e sigLep
 
 -- * nucleon opacities
 -- | Total nucleon opacity
-op_N :: Cell -> Energy -> Opacity
-op_N c e = (op_N_abs c e) + (op_N_elastic c e)
+opN :: Cell -> Energy -> Opacity
+opN c e = (opNAbs c e) + (opNElastic c e)
 
 -- | nucleon absorption
-op_N_abs :: Cell -> Energy -> Opacity
-op_N_abs c e_nu = mkOp (rhoN c) (Sigma.nu_N_abs e_nu)
+opNAbs :: Cell -> Energy -> Opacity
+opNAbs c e = mkOp (rhoN c) (Sigma.nuNAbs e)
 
 -- | nucleon elastic scattering
-op_N_elastic :: Cell -> Energy -> Opacity
-op_N_elastic c e_nu = mkOp (rhoN c) (Sigma.nu_N_elastic e_nu)
+opNElastic :: Cell -> Energy -> Opacity
+opNElastic c e = mkOp (rhoN c) (Sigma.nuNElastic e)
 
 -- * lepton opacities: these are complicated by the need to sample the
 -- lepton energy, as well as differentiation of reaction by 
@@ -33,29 +42,29 @@ op_N_elastic c e_nu = mkOp (rhoN c) (Sigma.nu_N_elastic e_nu)
 
 -- | total lepton opacity, using mean matter temperature as measure of 
 -- lepton energy
-op_lepton :: Cell -> Energy -> Sigma.Lepton -> Opacity
-op_lepton c e s = (op_e_minus c e s) + (op_e_plus c e s)
+opLepton :: Cell -> Energy -> Sigma.Lepton -> Opacity
+opLepton c e s = (opEMinus c e s) + (opEPlus c e s)
   
 -- | electron opacity
-op_e_minus :: Cell -> Energy -> Sigma.Lepton -> Opacity
-op_e_minus cell e_nu sigmas = mkOp rho_e sigma
-  where rho_e = rho_e_minus $ mat cell
+opEMinus :: Cell -> Energy -> Sigma.Lepton -> Opacity
+opEMinus cell e_nu sigmas = mkOp rho_e sigma
+  where rho_e = rhoEMinus $ mat cell
         sigma = (Sigma.e_minus sigmas) e_nu (tToEnergy cell)
 
 -- | positron opacity
-op_e_plus :: Cell -> Energy -> Sigma.Lepton -> Opacity
-op_e_plus cell e_nu sigmas = mkOp rho_p sigma
-  where rho_p = rho_e_plus $ mat cell
-        sigma = (Sigma.e_plus sigmas) e_nu (tToEnergy cell)
+opEPlus :: Cell -> Energy -> Sigma.Lepton -> Opacity
+opEPlus cell e_nu sigmas = mkOp rho_p sig
+  where rho_p = rhoEPlus $ mat cell
+        sig = (Sigma.e_plus sigmas) e_nu (tToEnergy cell)
 
 -- economize some of the unwrapping -> wrapping
 mkOp :: Density -> CrossSection -> Opacity
-mkOp r s = Opacity $ (rho r) * (mu s)
+mkOp r s = Opacity $ (rho r) * (sigma s)
 
 tToEnergy :: Cell -> Energy
 tToEnergy c = Energy . temp . tempE $ mat c
 
 rhoN :: Cell -> Density
-rhoN = rho_nucl . mat 
+rhoN = rhoNucl . mat 
 
 -- end of file
