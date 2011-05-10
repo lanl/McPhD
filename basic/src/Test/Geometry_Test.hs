9fe3dfbe96301341b2789fbdd2a59c4e6b1bb262
Timothy Kelley <tkelley@lanl.gov>
2011-05-10 16:46:20 -0600

    Added two more properties of Lorentz transforms.

--- basic/src/Test/Geometry_Test.hs
+++ basic/src/Test/Geometry_Test.hs
@@ -11,27 +11,38 @@ import SoftEquiv
 import qualified Physical as P
 import Debug.Trace
 
-prop_CM2Lab_Lab2CM_id el ol v  = (softEquiv e'' e tol)&&(softEquiv o'' o tol)
-  where (ecm,ocm)  = labToComoving el ol v
-        (el2,ol2)  = comovingToLab ecm ocm v
-        e   = P.e el
-        e'' = P.e el2
-        o   = P.dir ol
-        o'' = P.dir ol2
-        tol = 1e-12
 
+-- | LT with 0 velocity leaves inputs unchanged
+prop_v0_id_CM2Lab e o = comovingToLab e o (P.Velocity 0.0) == (e,o)
+
+
+-- | LT with 0 velocity leaves inputs unchanged
+prop_v0_id_Lab2CM e o = labToComoving e o (P.Velocity 0.0) == (e,o)
+
+
+-- | LT comoving->lab is inverse of LT lab->comoving
+prop_CM2Lab_Lab2CM_id el ol v  = (softEquiv e'' e tol)&&(softEquiv o'' o tol)
+    where (ecm,ocm)  = labToComoving el ol v
+          (P.Energy e'',P.Direction o'')  = comovingToLab ecm ocm v
+          e   = P.e el
+          o   = P.dir ol
+          tol = 1e-12
+
+
+-- | LT lab->comoving is (soft) inverse of LT comoving->lab 
 prop_Lab2CM_CM2Lab_id ecm ocm v  = (softEquiv e'' e tol)&&(softEquiv o'' o tol)
   where (el,ol)     = comovingToLab ecm ocm v
-        (ecm2,ocm2) = labToComoving el ol v
+        (P.Energy e'',P.Direction o'') = labToComoving el ol v
         e   = P.e ecm
-        e'' = P.e ecm2
         o   = P.dir ocm
-        o'' = P.dir ocm2
         tol = 1e-12
 
-tests = [testGroup "Lorentz Transforms" [
-           testProperty  "comovingToLab inverse of labToComoving" prop_CM2Lab_Lab2CM_id
-         , testProperty  "labToComoving inverse of comovingToLab" prop_Lab2CM_CM2Lab_id
-                     ] 
+tests = [testGroup "Lorentz Transforms" 
+         [
+           testProperty "comovingToLab inverse of labToComoving" prop_CM2Lab_Lab2CM_id
+         , testProperty "labToComoving inverse of comovingToLab" prop_Lab2CM_CM2Lab_id
+         , testProperty "LT with 0 velocity is id (comoving -> lab)" prop_v0_id_CM2Lab
+         , testProperty "LT with 0 velocity is id (lab -> comoving)" prop_v0_id_Lab2CM
+         ] 
         ]
 
