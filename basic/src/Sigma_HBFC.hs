a5a34c8f17164e615e20b7a0fce323bfd29ba6b4
Timothy Kelley <tkelley@lanl.gov>
2011-05-10 16:42:49 -0600

    underscores -> camel case.

--- basic/src/Sigma_HBFC.hs
+++ basic/src/Sigma_HBFC.hs
@@ -12,22 +12,22 @@ import Physical
 -- ------------------------
 -- | nucleon absorption/emission
 
-nu_N_abs :: Energy -> CrossSection
-nu_N_abs (Energy e) = CrossSection $ 9e-44 * e*e
+nuNAbs :: Energy -> CrossSection
+nuNAbs (Energy e) = CrossSection $ 9e-44 * e*e
 
 -- | nu--nucleon elastic scattering
-nu_N_elastic :: Energy -> CrossSection
-nu_N_elastic (Energy e) = CrossSection $ 1.7e-44*e*e
+nuNElastic :: Energy -> CrossSection
+nuNElastic (Energy e) = CrossSection $ 1.7e-44*e*e
 
 -- -- | nu-nucleus elastic scatter: attempts to account
 -- -- | for coherent scattering from bound nucleons
--- nu_N_Z_elastic
--- nu_N_Z_elastic (Energy e) N Z =
+-- nuNZ_elastic
+-- nuNZ_elastic (Energy e) N Z =
 --   CrossSection $ 1.7e-44*(N-0.08*Z)*(N-0.08*Z)/6.0*e*e
 
 -- | if not too concerned about the balance between n's & p's:
-nu_A_elastic :: Energy -> NucleonNumber -> CrossSection
-nu_A_elastic (Energy e) (NucleonNumber a) = CrossSection $ 1.7e-44*a*a/6.0*e*e
+nuAElastic :: Energy -> NucleonNumber -> CrossSection
+nuAElastic (Energy e) (NucleonNumber a) = CrossSection $ 1.7e-44*a*a/6.0*e*e
 
 
 -- NOTE: the lepton cross sections are complicated by dependence on
@@ -38,21 +38,21 @@ nu_A_elastic (Energy e) (NucleonNumber a) = CrossSection $ 1.7e-44*a*a/6.0*e*e
 -- * neutrino--lepton scattering
 -- ----------------------------
 -- | nu_e--e^- & nu_bar_e--e^+ scattering
-nu_e_e_minus :: Energy -> Energy -> CrossSection
-nu_e_e_minus  (Energy e_nu) (Energy e_e) = CrossSection $ 9.2e-45 * e_nu * e_e
+nuEEMinus :: Energy -> Energy -> CrossSection
+nuEEMinus  (Energy e_nu) (Energy e_e) = CrossSection $ 9.2e-45 * e_nu * e_e
 
 -- | nu_bar_e--e^- & nu_e--e^+ scattering
-nu_e_e_plus :: Energy -> Energy -> CrossSection
-nu_e_e_plus (Energy e_nu) (Energy e_e) = CrossSection $ 3.9e-45 * e_nu * e_e
+nuEEPlus :: Energy -> Energy -> CrossSection
+nuEEPlus (Energy e_nu) (Energy e_e) = CrossSection $ 3.9e-45 * e_nu * e_e
 
 
 -- | nu_x--e^- & nu_bar_x--e^+ scattering
-nu_x_e_minus :: Energy -> Energy -> CrossSection
-nu_x_e_minus  (Energy e_nu) (Energy e_e) = CrossSection $ 1.8e-45 * e_nu * e_e
+nuXEMinus :: Energy -> Energy -> CrossSection
+nuXEMinus  (Energy e_nu) (Energy e_e) = CrossSection $ 1.8e-45 * e_nu * e_e
 
 -- | nu_bar_x--e^- & nu_x--e^+ scattering
-nu_x_e_plus :: Energy -> Energy -> CrossSection
-nu_x_e_plus (Energy e_nu) (Energy e_e) = CrossSection $ 1.3e-45 * e_nu * e_e
+nuXEPlus :: Energy -> Energy -> CrossSection
+nuXEPlus (Energy e_nu) (Energy e_e) = CrossSection $ 1.3e-45 * e_nu * e_e
 
 
 
@@ -70,25 +70,31 @@ nu_x_e_plus (Energy e_nu) (Energy e_e) = CrossSection $ 1.3e-45 * e_nu * e_e
 data Lepton = Lepton {
     e_minus :: Energy -> Energy -> CrossSection
   , e_plus  :: Energy -> Energy -> CrossSection
-}
+} 
 
 nu_e = Lepton {
-    e_minus = nu_e_e_minus
-  , e_plus  = nu_e_e_plus
+    e_minus = nuEEMinus
+  , e_plus  = nuEEPlus
 }
 
 nu_e_bar = Lepton {
-    e_minus = nu_e_e_plus
-  , e_plus  = nu_e_e_minus
+    e_minus = nuEEPlus
+  , e_plus  = nuEEMinus
 }
 
 nu_x = Lepton {
-    e_minus = nu_x_e_minus
-  , e_plus  = nu_x_e_plus
+    e_minus = nuXEMinus
+  , e_plus  = nuXEPlus
 }
 
 nu_x_bar = Lepton {
-    e_minus = nu_x_e_plus
-  , e_plus  = nu_x_e_minus
+    e_minus = nuXEPlus
+  , e_plus  = nuXEMinus
 }
 
+-- really, I suppose we could do better
+instance Show Lepton where
+  show _ = "lepton"
+
+
+-- end of file
