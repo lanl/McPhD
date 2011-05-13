0504dbaebfca49a814d8d20a91ec209f3091bc5f
Timothy Kelley <tkelley@lanl.gov>
2011-05-13 15:51:10 -0600

    Added Collision tests.

--- basic/src/TestSuite.hs
+++ basic/src/TestSuite.hs
@@ -8,10 +8,12 @@ import Test.Mesh_test as Mesh
 -- import Test.MC_test as MC
 import Test.Geometry_Test as Geometry
 import Test.Opacity_Test as Opacity
+import Test.Collision_Test as Collision
 
 all_tests = [testGroup "Mesh tests"     Mesh.tests
-           , testGroup "Geometry Tests" Geometry.tests
+            , testGroup "Geometry Tests" Geometry.tests
             , testGroup "Opacity Tests" Opacity.tests
+            , testGroup "Collision Tests" Collision.tests
             -- , testGroup "Sphere1D tests" Sphere1D.tests
             -- , testGroup "MC tests"       MC.tests
             ]
