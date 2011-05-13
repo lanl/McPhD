3d4e5bd8a0dc01feea84e90ed9dbab4baefea86d
Timothy Kelley <tkelley@lanl.gov>
2011-05-13 15:12:55 -0600

    Changed lepton densities to number densities (NDensity).

--- basic/src/Main-1000.hs
+++ basic/src/Main-1000.hs
@@ -54,15 +54,15 @@ bigMesh =
      [Cell (Position 0) (Position 1)       Refl   Transp
            (Material (Opacity 0.1) (Opacity 2)
                      (Velocity 0) (Temperature 1)
-                     (Density 1.0) (Density 0.5) (Density 0))]
+                     (Density 1.0) (NDensity 0.5) (NDensity 0))]
      L.++
      [Cell (Position i) (Position $ i+1.0) Transp Transp
            (Material (Opacity 1) (Opacity 0.5)
                      (Velocity 0) (Temperature 2)
-                     (Density 1.0) (Density 0.5) (Density 0))
+                     (Density 1.0) (NDensity 0.5) (NDensity 0))
      | i <- [1..998]]
      L.++
      [Cell (Position 999) (Position 1000)  Transp Refl
            (Material (Opacity 1)   (Opacity 0.5)
                      (Velocity 0) (Temperature 2)
-                     (Density 1.0) (Density 0.5) (Density 0))]
+                     (Density 1.0) (NDensity 0.5) (NDensity 0))]
