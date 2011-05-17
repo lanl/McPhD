1d3c0cc71cfec3879a7db3f26f3032cc0dc3d7a0
Timothy Kelley <tkelley@lanl.gov>
2011-05-17 17:30:15 -0600

    Refactored Event as previously indicated: now there are three types of Event. Two include an enumeration variable indicating subtype.

--- basic/src/Event.hs
+++ basic/src/Event.hs
@@ -7,101 +7,44 @@ import Cell
 import Physical hiding (distance)
 
 -- | Events: what can happen to a particle on a Monte Carlo step.
---
--- Scatter:  some type of scatter; NB neutrions have more interaction
---           channels than photons: loks like ~6-8 per lepton, x2 for
---           anti-nu's
--- Absorb:   physically a subset of scattering, computationally different
---           in that absorption terminates a particle's flight
--- Transmit: reach a cell boundary and cross into a new mesh cell
--- Reflect:  reach a cell boundary and reflect from it
--- Escape:   reach a cell boundary and leave the problem domain
--- Census:   the particle's internal clock has reached the end of the
---           time step; it is banked for the next time step.
+-- Collision: interaction with the material medium. 
+-- Boundary: Crossing a mesh boundary. 
+-- Timeout: allows us to limit the time an individual particle is allowed to explore.
 
+data CollType  = NuclAbs | NuclEl | EMinusInel | EPlusInel deriving (Show,Eq)
 
-{-- QUESTION: In some sense, there are really three types here, along 
-with two enumerations. The types are 
-  Collision (NucleonAbsorb | ... | EPlusInelastic)
-  Boundary (Transmit | Reflect | Escape )
-  Census. 
-Would the following make more sense?
-
------
-data CollType  = NucleonAbsorb | NucleonElastic | EMinusInelastic | EPlusInelastic
-
-data BoundType = Transmit | Reflect | Escape
+data BoundType = Transmit | Reflect | Escape deriving (Show,Eq)
 
 data Event = 
-    Collision { dist   :: !Distance
-              , deltaP :: Momentum
-              , eDep   :: EnergyWeight
-              , CollType
+    Collision { cType :: CollType
+              , dist     :: !Distance   -- ^ distance travelled to collision
+              , pDep     :: Momentum    -- ^ momentum transferred (k_i - k_f)
+              , eDep     :: Energy      -- ^ energy transferred (E_i - E_f)
             }
-  | Boundary { dist :: !Distance
-             , face :: Face
-             , BoundType
+  | Boundary { bType :: BoundType
+             , dist      :: !Distance   -- ^ distance to boundary
+             , face      :: Face        -- ^ which face intersected
              }
-  | Census   { dist :: !Distance
-             , deltaP :: Momentum
-             }
------
-
-I'm curious, especially in the case of Opacity.sampleCollision, whether there 
-would be any benefit. 
--}
-data Event =
-    Scatter  { dist   :: !Distance            -- ^ distance travelled
-             , deltaP :: Momentum       -- ^ momentum deposited
-             , eDep   :: EnergyWeight   -- ^ energy deposited
-             }
-  | Absorb   { dist   :: !Distance            -- ^ distance travelled
-             , deltaP :: Momentum       -- ^ momentum deposited
-             , eDep   :: EnergyWeight   -- ^ energy deposited
-             }
-  | NucleonAbsorb  { dist   :: !Distance            -- ^ distance travelled
-                   , deltaP :: Momentum       -- ^ momentum deposited
-                   , eDep   :: EnergyWeight   -- ^ energy deposited
-                   }
-  | NucleonElastic { dist   :: !Distance            -- ^ distance travelled
-                   , deltaP :: Momentum       -- ^ momentum deposited
-                   , eDep   :: EnergyWeight   -- ^ energy deposited
-                   }
-  | EMinusInelastic { dist   :: !Distance            -- ^ distance travelled
-                    , deltaP :: Momentum       -- ^ momentum deposited
-                    , eDep   :: EnergyWeight   -- ^ energy deposited
-                    }
-  | EPlusInelastic  { dist   :: !Distance            -- ^ distance travelled
-                    , deltaP :: Momentum       -- ^ momentum deposited
-                    , eDep   :: EnergyWeight   -- ^ energy deposited
-                    }
-  | Transmit { dist   :: !Distance            -- ^ distance travelled
-             , face   :: Face           -- ^ which boundary
-             }
-  | Escape   { dist   :: !Distance            -- ^ distance travelled
-             , face   :: Face           -- ^ which boundary
-             }
-  | Reflect  { dist   :: !Distance            -- ^ distance travelled
-             , face   :: Face           -- ^ which boundary
-             }
-  | Census   { dist   :: !Distance            -- ^ distance travelled
-             , deltaP :: Momentum       -- ^ momentum deposited
-             }
+  | Timeout  { dist :: !Distance } -- ^ distance to timeout
   deriving (Show, Eq)
+
 -- REMARK: Only dist is strict. The reason is that we generate several
 -- events in each turn, and only select the one with the smallest
 -- distance. We don't actually want to perform the extra work for the
 -- events that are discarded.
+
 
 -- | Common type for all boundary event constructors.
 type BoundaryEvent = Distance -> Face -> Event
 
 -- | Does an event continue a random walk?
 isContinuing :: Event -> Bool
-isContinuing (Scatter  {}) = True
-isContinuing (Reflect  {}) = True
-isContinuing (Transmit {}) = True
-isContinuing _             = False
+isContinuing (Collision {cType = NuclEl})     = True
+isContinuing (Collision {cType = EMinusInel}) = True
+isContinuing (Collision {cType = EPlusInel})  = True
+isContinuing (Boundary {bType = Reflect})     = True
+isContinuing (Boundary {bType = Transmit})    = True
+isContinuing _                                = False
 
 -- | Distance to be travelled for the event
 distance :: Event -> Distance
