cd9cd9bc37bf2586e60f3e6c26ccf37db7375c57
Timothy Kelley <tkelley@lanl.gov>
2011-05-18 14:39:26 -0600

    Added two more properties: sampleDirection returns a direction between -1 and 1; distanceToBoundary is greater than 0.

--- basic/src/Test/Mesh_test.hs
+++ basic/src/Test/Mesh_test.hs
@@ -16,6 +16,8 @@ import Cell
 import Physical
 import PRNG
 
+-- helpers
+import Control.Applicative
 import Debug.Trace
 
 
@@ -35,9 +37,31 @@ psnLEHighBnd s c = do
       x  = pos p
   return $ x <= hb
 
+-- * sampleDirection: direction cosine must be > -1.0 and < 1.0
+dirBnded :: Sphere1D -> Rnd Bool
+dirBnded s = do 
+  (Direction o) <- sampleDirection s
+  return $ -1.0 <= o && o <= 1.0
+
+
+-- QUESTION: is there a cleaner way to couple types?
+
+-- | distance to boundary >= 0. Implementation is circuitous to ensure that the 
+-- sampled position is within the Cell boundaries. 
+dToBoundGE0 :: Sphere1D -> Cell -> Direction -> Rnd Bool
+dToBoundGE0 s c o = do
+  xi <- random
+  let (l,h) = (pos . lowB $ c, pos . highB $ c) 
+      x     = l + (h - l) * xi
+      p     = Position x
+      (Distance d, _) = distanceToBoundary s c p o
+  return $ d >= 0
 
 tests = [testGroup "Sphere1D" 
          [ testProperty "samplePosInCell: x >= cell lower bound" psnGELowBnd
          , testProperty "samplePosInCell: x <= cell upper bound" psnLEHighBnd
+         , testProperty "sampleDirection: -1 < omega < 1" dirBnded
+         , testProperty "distanceToBoundary: d >= 0" dToBoundGE0
+
          ]
         ]
