11681dfbc25813903b3c8ea93f5d103b73ad79c4
Timothy Kelley <tkelley@lanl.gov>
2011-05-18 12:59:31 -0600

    Provided two properties for samplePositionInCell.

--- basic/src/Test/Mesh_test.hs
+++ basic/src/Test/Mesh_test.hs
@@ -6,13 +6,38 @@ import Test.Framework (testGroup)
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.HUnit
+import Test.QuickCheck
+import Test.Arbitraries
 
 -- The library under test
+import Sphere1D
 import Mesh
+import Cell
+import Physical
+import PRNG
+
+import Debug.Trace
 
 
--- | Property: Crossing a face and crossing it back return you to the same cell.
+-- * samplePositionInCell: (1) pos >= lower boundary, 
+-- (2) pos <= upper boundary
+psnGELowBnd :: Sphere1D -> Cell -> Rnd Bool
+psnGELowBnd s c = do
+  p <- samplePositionInCell s c
+  let lb = pos . lowB $ c
+      x  = pos p
+  return $ x >= lb
+
+psnLEHighBnd :: Sphere1D -> Cell -> Rnd Bool
+psnLEHighBnd s c = do
+  p <- samplePositionInCell s c
+  let hb = pos . highB $ c
+      x  = pos p
+  return $ x <= hb
 
 
-
-tests = []
+tests = [testGroup "Sphere1D" 
+         [ testProperty "samplePosInCell: x >= cell lower bound" psnGELowBnd
+         , testProperty "samplePosInCell: x <= cell upper bound" psnLEHighBnd
+         ]
+        ]
