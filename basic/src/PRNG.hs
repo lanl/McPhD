06019ddb8cccd2fd10bb0d63f8db87181862e2f3
Andres Loeh <andres@well-typed.com>
2011-05-18 17:57:16 +0200

    Suggestion for how to test Rnd-properties.

--- basic/src/PRNG.hs
+++ basic/src/PRNG.hs
@@ -4,6 +4,7 @@ module PRNG where
 import Control.Monad.State.Strict
 import Control.Monad.Identity
 import System.Random as R
+import Test.QuickCheck
 
 newtype RNG = RNG StdGen
 
@@ -17,6 +18,14 @@ testRNG = mkRNG 42
 
 newtype Rnd a = Rnd (State RNG a)
   deriving (Monad, MonadState RNG)
+
+-- We can make a Rnd-value into a testable value.
+-- In the 'property' method, we specify how to initialize
+-- the random number generator in this case. Currently, we
+-- take a fixed RNG here! But we could and probably should
+-- initialize in a different way.
+instance Testable a => Testable (Rnd a) where
+  property s = property (fst (runRnd testRNG s))
 
 random :: Random a => Rnd a
 random = Rnd (StateT (\ (RNG g) ->
