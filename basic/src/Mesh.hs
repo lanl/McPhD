933f047982bd5451358bbf3b35b7584938de5e51
Timothy Kelley <tkelley@lanl.gov>
2011-05-18 12:49:16 -0600

    Changed samplePositionInCell to take a Cell instead of a CellIdx: this removes the problem of consistency between a cell index and the mesh. It also makes the mesh parameter spurious, except for dispatch for typeclass.

--- basic/src/Mesh.hs
+++ basic/src/Mesh.hs
@@ -10,7 +10,7 @@ import Physical
 
 class Mesh m where
   samplePosition       :: m -> Rnd (Position, CellIdx)
-  samplePositionInCell :: m -> CellIdx -> Rnd Position
+  samplePositionInCell :: m -> Cell -> Rnd Position
   sampleDirection      :: m -> Rnd Direction
   distanceToBoundary   :: m -> CellIdx -> Position -> Direction -> (Distance, Face)
   cells                :: m -> Vector Cell
