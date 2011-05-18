695c14508212f1cc5f2fd1b4a200043d06ab675d
Timothy Kelley <tkelley@lanl.gov>
2011-05-18 09:12:23 -0600

    Added (as yet untested!) genParticlesInCells & implementing functions.

--- basic/src/Source.hs
+++ basic/src/Source.hs
@@ -28,3 +28,32 @@ genParticle msh rng =
         ew = 1
     rng'   <- get
     return (Particle x d t e ew c rng')
+
+-- | generate a given number of particles in each cell
+genParticlesInCells :: Mesh m => m -> RNG -> [(CellIdx,Int)] -> [Particle]
+genParticlesInCells msh rng nPerCell = 
+  join $ map (genParticlesInCell msh rng) nPerCell
+
+-- | generate a given number of particles in one cell
+genParticlesInCell :: Mesh m => m -> RNG -> (CellIdx,Int) -> [Particle]
+genParticlesInCell msh rng (cidx,n) =
+  let rngs = take n (unfoldr (Just . split) rng)
+  in  map (genCellParticle msh cidx) rngs
+
+-- | Generate a single random particle.
+genCellParticle :: Mesh m => m -> CellIdx -> RNG -> Particle
+genCellParticle msh cidx rng =
+  fst $ runRnd rng $ do
+    d <- sampleDirection msh
+    x <- samplePositionInCell msh cidx
+    let 
+        t  :: Time
+        t  = 1
+        e  :: Energy
+        e  = 1
+        ew :: EnergyWeight
+        ew = 1
+    rng'   <- get
+    return (Particle x d t e ew cidx rng')
+
+-- 
