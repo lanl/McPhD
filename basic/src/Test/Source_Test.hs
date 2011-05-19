22a6e6c1f68c9d4db5bfc1bac9cea02f15f9cc02
Timothy Kelley <tkelley@lanl.gov>
2011-05-19 16:29:24 -0600

    Added HUnit test cases for rejection function. Partly just trying to see how to do it. Cases derived  from Mathematica.

--- basic/src/Test/Source_Test.hs
+++ basic/src/Test/Source_Test.hs
@@ -12,11 +12,15 @@ import Test.Arbitraries
 -- The library under test
 import Source
 import Physical
-import PRNG
+-- import PRNG
 
 -- helpers
-import Control.Applicative
-import Debug.Trace
+-- import Control.Applicative
+import SoftEquiv
+import Data.List (zip4)
+
+-- test case data
+import Test.Source_Test_Cases
 
 -- * energy sampling properties
 -- | The power law rejector is bounded: specialized to a = 2.0 
@@ -30,9 +34,30 @@ rejectorBnded :: (Positive Double) -> Alpha -> Bool
 rejectorBnded (Positive x) (Alpha a) = r >= 0.0 && r <= 1.0
   where r = rejector x a
 
+-- * energy sampling test cases
+rejData = zip4 [1..(length rejxs)] rejxs rejas rejs :: 
 
-tests = [testGroup "energy sampling" 
+rejStr :: Int -> String
+rejStr i = "rej. case " ++ show i 
+
+rejFailStr :: Int -> FP -> FP -> FP -> String
+rejFailStr i x a r = "rejection case " ++ show i ++ " failed, x: " ++ show x ++
+                     ", a: " ++ show a ++ ", expected: " ++ show r
+
+rejTest :: Int -> FP -> FP -> FP -> Assertion
+rejTest i x a r = assertBool (rejFailStr i x a r) (softEquiv (rejector x a) r 1e-15)
+
+rejCases = [testCase (rejStr i) (rejTest i x a r) | (i,x,a,r) <- rejData]
+
+
+
+tests = [testGroup "energy sampling properties" 
          [ testProperty "rejector: 0 <= r <= 1 (alpha = 2)" rejectorBndedA2
          , testProperty "rejector: 0 <= r <= 1 (alpha arb)" rejectorBnded
          ]
+        , testGroup "energy sampling cases: rejector" rejCases
         ]
+
+
+
+-- end of file
