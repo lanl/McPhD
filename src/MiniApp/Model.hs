52b787d801f4a7a09019db83a966f02fb08de9bc
Michael Buksas <mwbuksas@lanl.gov>
2011-06-08 07:51:52 -0600

    Adding more comments and fixing some type issues.

--- src/MiniApp/Model.hs
+++ src/MiniApp/Model.hs
@@ -6,19 +6,27 @@ module MiniApp.Model where
 Defines the Model, which is the combination of data and operations
 which are sufficient to determine the next event in a particle history.
 
+Note that while this model is written to be polymorphic over the mesh,
+this is not required in general.
+
 -}
 
 import Data.Array.IArray
 
 import Mesh.Classes
 import qualified Particle.Classes as P
+import qualified MonteCarlo as MC
 
 import Properties
 
 import MiniApp.Particle
 import MiniApp.Events
 import MiniApp.Physics
-import MiniApp.Outcome
+
+
+-- Aliases for the MonteCarlo types we need.
+type Outcome m    = MC.Outcome (Event m) (Particle m)
+type Contractor m = MC.Contractor (Model m) (Particle m) (Event m)
 
 
 -- | The physical model. Consists of a mesh, space properties indexed
@@ -33,38 +41,29 @@ data (Mesh m) => Model m = Model {
 localPhysics :: (Mesh m) => Model m -> Particle m -> Physics (MeshSpace m)
 localPhysics model particle = (physics model) ! (cell particle)
 
-
--- | Advance the particle one step, according to the model.
---
--- This function could be quite general, if given a list of functions
--- which produce outcomes. (Contractors)
-step :: (Mesh m) => Model m -> Particle m -> (Event m, Particle m)
-step model particle =
-    -- | Compute the outcome of physics, the mesh and the timestep.
-    let outcomes = [ physicsOutcome (localPhysics model particle) particle
-                   , timeStepOutcome model particle
-                   , meshOutcome model particle]
-    -- | Smallest distance wins.
-    in result (foldl1 min outcomes)
-
-
--- * Functions which compute particular kinds of outcomes, resulting
--- from interaction with the mesh, the timestep and the medium. Each
--- of these has the same type:
+-- * Functions which compute outcomes, resulting from interaction with
+-- the mesh, the timestep and the medium. Each of these has the same
+-- type, called Contractor:
 --
 --   Model m -> Particle m -> Outcome m
 
 -- | Compute an Outcome for reaching the timestep end.
-timeStepOutcome :: (Mesh m) => Model m -> Particle m -> Outcome m
-timeStepOutcome model particle =
+timeStepContractor :: (Mesh m) => Contractor m
+timeStepContractor model particle =
     let time_left = t_final model - time particle
         distance  = gettingTo time_left (speed particle)
         particle' = P.move particle distance
-    in Outcome distance Timeout particle'
+    in MC.Outcome distance Timeout particle'
 
 -- | Dispatch to the mesh's distance to boundary functions.
-meshOutcome :: (Mesh m) => Model m -> Particle m -> Outcome m
-meshOutcome model particle = undefined
+meshContractor :: (Mesh m) => Contractor m
+meshContractor = undefined
 
 
--- materialOutcome :: (Mesh m) => Contractor
+materialContractor :: (Mesh m) => Contractor m
+materialContractor = undefined
+
+
+-- | A list of contractors that we hand to the step function.
+contractors :: (Mesh m) => [Contractor m]
+contractors = [timeStepContractor, meshContractor, materialContractor]
