ff25b28bd469815464bb4ba831998ae66e94c2ba
Timothy Kelley <tkelley@lanl.gov>
2011-06-10 10:42:22 -0600

    Made straight the execution paths.
    sampleCollision now returns a fully-formed Collision, along with a new direction; it transforms between lab and co-moving frames and computes deposition in the lab frame.
    
    sampleCollisionComoving marshals a new collision type, energy, and direction, all in the comoving frame.
    
    selectEvent selects the collision event.
    
    sampleDirectionIso has been removed (Mesh.sampleDirection is now Mesh.sampleDirectionIso).

--- basic/src/Collision.hs
+++ basic/src/Collision.hs
@@ -22,7 +22,8 @@ import PRNG
 dCollide :: Cell -> Energy -> Direction ->
             Sigma.Lepton -> URD -> Distance
 dCollide c e o sig xi = dCollideComoving c ecom sig xi
-  where (ecom,_) = labToComoving e o (mvel $ mat c)
+  where (ecom,_) = labToComoving e o v
+        v = (mvel $ mat c)
 
 
 -- | Distance to generic collision: the lesser of -log(\xi/\simga_t) and huge
@@ -36,44 +37,59 @@ dCollideComoving c e sig (URD xi) = Distance $ min (- (log xi) / sig_c) huge
   where sig_c = mu $ opCollide c e sig
 
 
--- | sampleCollision: transform to comoving, then use sampleCollisionComoving
-sampleCollision :: Cell -> Energy -> Direction -> Sigma.Lepton ->
-                   URD -> Distance -> Momentum -> Energy -> E.Event
-sampleCollision c e o sig xi  = sampleCollisionComoving c ecom sig xi
-  where (ecom,_) = labToComoving e o (mvel $ mat c)
+-- | sampleCollision: 
+-- (1) transform to comoving frame,  
+-- (2) use sampleCollisionComoving to get collision type, new
+--     energy, and new direction,
+-- (3) transform new energy and momentum back to the lab frame,
+-- (4) record energy and momentum deposition in the lab frame.
+sampleCollision :: Mesh m => m -> Cell -> Energy -> Direction -> 
+                   Sigma.Lepton -> Distance -> EnergyWeight ->
+                   Rnd (E.Event,Direction)
+sampleCollision m cll 
+                elin@(Energy eli) olin@(Direction oli)
+                sig dCol (EnergyWeight wt) = do
+  let v       = (mvel $ mat cll)
+      (eci,_) = labToComoving elin olin v
+  (collType,ecf,ocf) <- sampleCollisionComoving m cll eci sig 
+  let (Energy elf, ol'@(Direction olf)) = comovingToLab ecf ocf v
+      edep = Energy $ wt * (eli - elf)
+      pdep = Momentum $ wt/c*(eli*oli - elf*olf) 
+  return (E.Collision collType dCol pdep edep,ol')
+
+
+-- | sample a collision event in the comoving frame: Given the cell, 
+-- neutrino energy (comoving), and a uniform random deviate, select
+-- a scattering event. Determine final energy and momentum: at present,
+-- scattering is elastic and isotropic in comoving frame.
+sampleCollisionComoving :: Mesh m => m -> Cell -> Energy -> Sigma.Lepton -> 
+                          Rnd (E.CollType,Energy,Direction)
+sampleCollisionComoving m c e sig = do
+  xi <- random
+  ocf <- sampleDirectionIso m
+  let ecf = e
+      evt = selectEvent c e sig (URD xi)
+  return (evt, ecf, ocf)
 
 
 -- | sample a collision event in the comoving frame: Given the cell, 
 -- neutrino energy (comoving), and a uniform random deviate, select
 -- a scattering event. Assign distance, momentum, and energy weight
 -- to that event. 
-sampleCollisionComoving :: Cell -> Energy -> Sigma.Lepton -> URD -> 
-                           Distance -> Momentum -> Energy -> E.Event
-sampleCollisionComoving c e sig (URD xi) 
-  | xi < pNAbs     = E.Collision E.NuclAbs     -- the order here must match the order 
-  | xi < pNElastic = E.Collision E.NuclEl      -- in eventProbs (ultimately that comes
-  | xi < pEMinus   = E.Collision E.EMinusInel  -- from opacities).
-  | xi < pEPlus    = E.Collision E.EPlusInel
+selectEvent :: Cell -> Energy -> Sigma.Lepton -> URD -> E.CollType
+selectEvent c e sig (URD xi) 
+  -- the order here must match the order in eventProbs
+  -- (that in turn comes from opacities).
+  | xi < pNAbs     = E.NuclAbs 
+  | xi < pNElastic = E.NuclEl 
+  | xi < pEMinus   = E.EMinusInel
+  | xi < pEPlus    = E.EPlusInel 
   | otherwise      = error $ "Unable to assign event, xi = " ++ show xi ++ 
                      ", max event prob = " ++ show pEPlus
   where [pNAbs, pNElastic, pEMinus, pEPlus] = eventProbs c e sig
 
 
-
--- | sample direction isotropically in the comoving frame. This wraps a 
--- call to Mesh.sampleDirection in the appropriate Lorentz transforms. 
-sampleDirectionIso :: Mesh m => m -> Cell -> 
-                      Energy ->      -- ^ lab frame
-                      Direction ->   -- ^ lab frame
-                      Rnd Direction  -- ^ new direction, lab frame
-sampleDirectionIso m c e o = do
-  let  (ecom,_)  = labToComoving e o v
-       v         = (mvel $ mat c)
-  ocom' <- sampleDirection m
-  return (snd $ comovingToLab ecom ocom' v)
-
--- | Compute probabilities of scattering events, arranged as a discrete cumulative
--- distribution.  
+-- | Cumulative probabilities of scattering events.  
 eventProbs :: Cell -> Energy -> Sigma.Lepton -> [FP]
 eventProbs c e sig = scanl1 (+) $ map (/ sum ops) ops -- p_i = op_i/(sum_j op_j)
   where ops = mu <$> opacities c e sig
