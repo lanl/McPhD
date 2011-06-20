4c6b63f6c7a7800df2668483ff8cd23b944cc93c
Timothy Kelley <tkelley@lanl.gov>
2011-06-20 16:43:49 -0600

    Cell and BoundaryCondition now derive Eq.

--- basic/src/Cell.hs
+++ basic/src/Cell.hs
@@ -4,7 +4,7 @@ import Physical
 import Material
 
 data BoundaryCondition = Vac | Refl | Transp 
-  deriving Show
+  deriving (Show,Eq)
 
 data Cell = Cell {
     lowB   :: Position
@@ -13,7 +13,7 @@ data Cell = Cell {
   , highBC :: BoundaryCondition
   , mat    :: Material 
   }
-  deriving Show
+  deriving (Show, Eq)
 
 data Face = Lo | Hi 
   deriving (Eq, Show)
